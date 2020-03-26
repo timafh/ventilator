8e0d82c538b3ad6ede9b5f1230c06eca0678ca99
Tim Afholderbach <github-timafh@codeprofis.eu>
2020-03-26 23:09:44 +0100

    Set git to case sensitive - recommit last commit

---
+++ ventilator_control/src/Components/Configuration/Configuration.h
@@ -0,0 +1,22 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#include <stdint.h>
+
+// TODO: Maybe motor speed-pressure pair values should also be stored in EEPROM to keep the motor configuration away from the source code?
+
+/**
+ * @brief Configuration structure to read from eeprom or flash
+ */
+typedef struct Configuration {
+    uint8_t debugMode : 1;
+    uint8_t motorEnabled : 1;
+    uint8_t spare : 6;
+
+    uint8_t mode;
+
+    uint8_t revision;
+    uint16_t crc;
+} Configuration;
+
+#endif
---
+++ ventilator_control/src/Components/DeviceManagement/DeviceInfoManager.cpp
@@ -0,0 +1,34 @@
+#include "DeviceInfoManager.h"
+
+/**
+ * Reads the device information from the EEPROM and verifies the CRC checksum
+ * The read data is stored in deviceInfo.
+ * 
+ * Returns true if the device information was readable and false if not.
+ * In the event of an error, standard data (serial number XXXXXXXXX and date of manufacture 19900101)
+ * is stored in deviceInfo.
+ * 
+ * @return true sucess
+ * @return false not successful
+ */
+bool DeviceInfoManager::ReadDeviceInfo() {
+    uint8_t magicByte = EEPROM.read(DEVICE_INFO_MAGIC_BYTE_ADDRESS);
+    if(magicByte != 0x01) {
+
+        strcpy(this->deviceInfo->serialNumber, "XXXXXXXXXX");
+        strcpy(this->deviceInfo->productionDate, "19900101");
+        this->deviceInfo->crc = 0;
+        return false;
+    } else {
+        EEPROM.get(DEVICE_INFO_ADDRESS, this->deviceInfo);
+        FastCRC16 CRC16;
+        uint16_t calculatedCRC =  CRC16.ccitt((uint8_t*)this->deviceInfo, sizeof(DeviceInfo) - sizeof(uint16_t));
+        if(this->deviceInfo->crc != calculatedCRC) {
+            strcpy(this->deviceInfo->serialNumber, "XXXXXXXXXX");
+            strcpy(this->deviceInfo->productionDate, "19900101");
+            this->deviceInfo->crc = 0;
+            return false;
+        }
+        return true;
+    }  
+}
