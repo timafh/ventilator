ba6cc6c620957425f9ad688733fd8547db6a9f12
Tim Afholderbach <github-timafh@codeprofis.eu>
2020-03-26 20:58:11 +0100

    Commenting ConfigurationManager in header

--- ventilator_control/src/Components/Configuration/configuration.h
+++ ventilator_control/src/Components/Configuration/configuration.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+// TODO: Maybe motor speed-pressure pair values should also be stored in EEPROM to keep the motor configuration away from the source code?
+
 /**
  * @brief Configuration structure to read from eeprom or flash
  */
@@ -12,7 +14,6 @@ typedef struct Configuration {
     uint8_t spare : 6;
 
     uint8_t mode;
-
 
     uint8_t revision;
     uint16_t crc;
--- ventilator_control/src/Components/Configuration/configurationmanager.h
+++ ventilator_control/src/Components/Configuration/configurationmanager.h
@@ -11,8 +11,7 @@
 #include "configuration.h"
 
 #define MAGIC_BYTE_ADDRESS 0
-#define CONFIGURATION_ADDRESS 1
-#define DEVICEINFORMATION_ADDRESS CONFIGURATION_ADDRESS+sizeof(Configuration)
+#define CONFIGURATION_ADDRESS MAGIC_BYTE_ADDRESS + 1
 
 
 
@@ -27,13 +26,54 @@ class ConfigurationManager {
 
         ConfigurationManager();
 
+        /**
+         * @brief Reads configuration from EEPROM and returns status
+         * Also checks if the magic byte at position MAGIC_BYTE_ADDRESS
+         * can be found and initializes the EEPROM with default values if not.
+         * 
+         * @return ConfigurationManager::Error 
+         */
         ConfigurationManager::Error ReadConfiguration();
+
+        /**
+         * @brief Writes configuration to EEPROM and directly verifies writing process using CRC.
+         * 
+         * @return ConfigurationManager::Error 
+         */
         ConfigurationManager::Error WriteConfiguration();
+
+        /**
+         * @brief Verifies the crc of the current configuration object.
+         * 
+         * @return ConfigurationManager::Error 
+         */
         ConfigurationManager::Error VerifyConfiguration();
+        
+        ~ConfigurationManager() {
+            configuration = nullptr;
+        }
 
     private:
+        /**
+         * @brief Calculates CRC for the current configuration object without its CRC
+         * 
+         * @return uint16_t 
+         */
         uint16_t CalculateCRC();
+
+        /**
+         * @brief Checks if the EEPROM was written before
+         * An 0x01 byte is written MAGIC_BYTE_ADDRESS of the EEPROM after the first writing process.
+         * If the byte at MAGIC_BYTE_ADDRESS not equals 0x01, we assume that the EEPROM has not yet been written.
+         * 
+         * @return true if the magic byte was found
+         * @return false if not
+         */
         bool CheckIfWrittenBefore();
+
+        /**
+        * @brief Writes the magic byte 0x01 at MAGIC_BYTE_ADDRESS of the EEPROM. * 
+        */
         void WriteMagicByte();
 
     public:
