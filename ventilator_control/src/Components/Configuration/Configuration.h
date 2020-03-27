3513681ba602012bf754c49ab6654012c3e28b99
Tim Afholderbach <github-timafh@codeprofis.eu>
2020-03-27 12:44:49 +0100

    Add speedState as configuration variable

--- ventilator_control/src/Components/Configuration/Configuration.h
+++ ventilator_control/src/Components/Configuration/Configuration.h
@@ -8,12 +8,14 @@
 /**
  * @brief Configuration structure to read from eeprom or flash
  */
-typedef struct Configuration {
+typedef struct Configuration
+{
     uint8_t debugMode : 1;
     uint8_t motorEnabled : 1;
     uint8_t spare : 6;
 
     uint8_t mode;
+    uint8_t speedState;
 
     uint8_t revision;
     uint16_t crc;
