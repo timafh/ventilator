e5bb23f7d1ab90af8ec264ea9996310fa2696073
Tim Afholderbach <github-timafh@codeprofis.eu>
2020-03-27 14:42:51 +0100

    introduce global variables and global pin description

---
+++ ventilator_control/src/globals.h
@@ -0,0 +1,9 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "pindesc.h"
+#include "Components/Configuration/ConfigurationManager.h"
+
+extern ConfigurationManager configurationManager;
+
+#endif
---
+++ ventilator_control/src/pindesc.h
@@ -0,0 +1,6 @@
+#ifndef PINDESC_H
+#define PINDESC_H
+
+#define INFO_LED 13
+
+#endif
