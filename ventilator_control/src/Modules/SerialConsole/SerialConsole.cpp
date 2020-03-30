a8f1f69fef2753c6176e6dac7a9402c545933171
Tim Afholderbach <github-timafh@codeprofis.eu>
2020-03-30 14:06:24 +0200

    WIP: Implenting a printf like function DebugOutf in SerialConsole

--- ventilator_control/src/Modules/SerialConsole/SerialConsole.cpp
+++ ventilator_control/src/Modules/SerialConsole/SerialConsole.cpp
@@ -9,8 +9,23 @@ void SerialConsole::tick()
 {
 }
 
-void SerialConsole::DebugOutf(const char *scope, const char *format, ...)
+void SerialConsole::DebugOutf(const char *scope, char *format, ...)
 {
+    char *traverse;
+    char *output = malloc(1);
+    unsigned int i;
+    char *s;
+
+    va_list arg;
+    va_start(arg, format);
+
+    for (traverse = format; *traverse != '\0'; traverse++)
+    {
+        while (*traverse != '%')
+        {
+            void *tmp = realloc(output, strlen(output) + 1);
+        }
+    }
 }
 
 void SerialConsole::DebugOut(const char *scope, const char *message)
--- ventilator_control/src/Modules/SerialConsole/SerialConsole.h
+++ ventilator_control/src/Modules/SerialConsole/SerialConsole.h
@@ -2,6 +2,9 @@
 #define SERIALCONSOLE_H
 
 #include <Arduino.h>
+#include <stdarg.h>
+#include <stdio.h>
+
 #include "../../Interfaces/BaseTickableComponent.h"
 
 #include "../../pindesc.h"
@@ -12,7 +15,7 @@ public:
     void setup();
     void tick();
     void DebugOut(const char *scope, const char *message);
-    void DebugOutf(const char *scope, const char *format, ...);
+    void DebugOutf(const char *scope, char *format, ...);
 };
 
 extern SerialConsole serialConsole;
