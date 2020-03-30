cd183ccd02031f61fcae005093aee27c78d69e97
Tim Afholderbach <github-timafh@codeprofis.eu>
2020-03-30 13:55:09 +0200

    Begin implementing serial console.

--- ventilator_control/src/Modules/SerialConsole/SerialConsole.cpp
+++ ventilator_control/src/Modules/SerialConsole/SerialConsole.cpp
@@ -1,1 +1,25 @@
 #include "SerialConsole.h"
+
+void SerialConsole::setup()
+{
+    Serial.begin(SERIAL_BAUDRATE);
+}
+
+void SerialConsole::tick()
+{
+}
+
+void SerialConsole::DebugOutf(const char *scope, const char *format, ...)
+{
+}
+
+void SerialConsole::DebugOut(const char *scope, const char *message)
+{
+#ifdef DEBUG
+    size_t length = strlen(scope) + strlen(message);
+    char *msg = (char *)malloc(length + 11);
+    sprintf(msg, "[DEBUG][%s]: %s", scope, message);
+    Serial.println(msg);
+    free(msg);
+#endif
+}
--- ventilator_control/src/Modules/SerialConsole/SerialConsole.h
+++ ventilator_control/src/Modules/SerialConsole/SerialConsole.h
@@ -4,11 +4,17 @@
 #include <Arduino.h>
 #include "../../Interfaces/BaseTickableComponent.h"
 
+#include "../../pindesc.h"
+
 class SerialConsole : BaseTickableComponent
 {
-
+public:
     void setup();
     void tick();
+    void DebugOut(const char *scope, const char *message);
+    void DebugOutf(const char *scope, const char *format, ...);
 };
+
+extern SerialConsole serialConsole;
 
 #endif
